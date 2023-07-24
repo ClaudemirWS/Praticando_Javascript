8f71130d749a37bda13c2fd2d94b56f600fabbf5
Claudemir Souza <claudemirwss@Gmail.com>
2023-07-24 14:12:50 -0300

    update atividade3

--- Curso Técnico - SENAC/Atividade 3/script.js
+++ Curso Técnico - SENAC/Atividade 3/script.js
@@ -16,16 +16,36 @@ function Carregar() {
         //Torna a opção CARTÃO visível e mantém oculta a PIX
         document.getElementById('cartao').style.display = 'block';
         document.getElementById('pix').style.display = 'none';
+        var numeroInput = document.getElementById('numero');
+        var imagemCartao = document.getElementById('imagemCartao');
+
+        numeroInput.addEventListener('input', function () {
+            var valorNumero = numeroInput.value;
+            if (valorNumero.startsWith('1234')) {
+                // Defina o caminho para a primeira imagem aqui
+                imagemCartao.src = '/imagens/bandeira1.png';
+                mensagemInvalido.style.display = 'none';
+            } else if (valorNumero.startsWith('4321')) {
+                // Defina o caminho para a segunda imagem aqui
+                imagemCartao.src = '/imagens/bandeira2.png';
+                mensagemInvalido.style.display = 'none';
+            } else {
+                imagemCartao.src = '';
+                mensagemInvalido.textContent = 'Número do cartão inválido';
+                mensagemInvalido.style.display = 'block';
+            }
+        });
     } else if (opcaoSelecionada == 'pix') {
+        var valorTotal = document.getElementById('totalPix');
         //Torna a opção PIX visível e mantém oculta a CARTÃO
         document.getElementById('pix').style.display = 'block';
         document.getElementById('cartao').style.display = 'none';
         //Calcular o valor total com desconto (10%)
-        var valorTotal = document.getElementById("total");
         var valorComDesconto = valor.value - (valor.value * 10 / 100);
         valorTotal.innerHTML = "Total: R$ " + valorComDesconto.toFixed(2); //Duas casas decimais 
         valor.value = '';
     } else if (opcaoSelecionada == null) {
         alert("Selecione uma forma de pagamento!");
     }
+
 }
