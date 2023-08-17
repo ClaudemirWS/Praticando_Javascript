32e3d1a5e62780058a9fbe06d18d25f535074b5b
Claudemir Souza <claudemirwss@Gmail.com>
2023-08-17 08:33:07 -0300

    Pequena melhoria na tabuada

--- Curso em Vídeo/Exercicios/Tabuada/script.js
+++ Curso em Vídeo/Exercicios/Tabuada/script.js
@@ -6,8 +6,11 @@ function tabuada() {
 
     num = Number(num.value);
 
-    for (let i = 1; i <= 10; i++) {
-        tab.innerHTML += `${num} x ${i} = ${num*i}\n`;
+    if (num != 0) {
+        for (let i = 1; i <= 10; i++) {
+            tab.innerHTML += `${num} x ${i} = ${num * i}\n`;
+        }
+    } else {
+        alert('Digite um número maior do que 0.')
     }
-
 }
